fff99972c9718bd6f9f401eb2c7d411fae5332e9
hendrism <hendrism@gmail.com>
2025-08-03 20:33:30 -0400

    chore: add testing library and update tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders a placeholder title', () => {
+  render(<div>Merchant's Morning</div>);
+  expect(screen.getByText(/Merchant's Morning/i)).toBeInTheDocument();
 });
