bba3d1fa5936c091acad23848cb5dfd22be5f058
hendrism <hendrism@gmail.com>
2025-08-05 19:03:25 -0400

    Add new boxes and categorized materials

--- src/constants/boxes.js
+++ src/constants/boxes.js
@@ -2,4 +2,7 @@ export const BOX_TYPES = {
   bronze: { name: 'Bronze Box', cost: 20, materialCount: [5, 7], rarityWeights: { common: 75, uncommon: 25, rare: 0 } },
   silver: { name: 'Silver Box', cost: 45, materialCount: [6, 9], rarityWeights: { common: 45, uncommon: 45, rare: 10 } },
   gold: { name: 'Gold Box', cost: 85, materialCount: [7, 10], rarityWeights: { common: 25, uncommon: 55, rare: 20 } },
+  platinum: { name: 'Platinum Box', cost: 140, materialCount: [8, 12], rarityWeights: { common: 15, uncommon: 50, rare: 35 } },
+  diamond: { name: 'Diamond Box', cost: 200, materialCount: [9, 13], rarityWeights: { common: 10, uncommon: 45, rare: 45 } },
+  mythic: { name: 'Mythic Box', cost: 300, materialCount: [10, 15], rarityWeights: { common: 0, uncommon: 30, rare: 70 } },
 };
