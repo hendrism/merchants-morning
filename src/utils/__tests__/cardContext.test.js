bfd2963fbf84bb3967dfa8570354ee6b08ef1499
hendrism <hendrism@gmail.com>
2025-08-08 10:58:40 -0400

    Ensure customer queue visible during shopping

---
+++ src/utils/__tests__/cardContext.test.js
@@ -0,0 +1,10 @@
+import { getDefaultCardStatesForPhase } from '../cardContext';
+import { PHASES } from '../../constants';
+
+describe('card context defaults', () => {
+  test('customer queue is visible in shopping phase', () => {
+    const states = getDefaultCardStatesForPhase(PHASES.SHOPPING);
+    expect(states.customerQueue.hidden).toBe(false);
+    expect(states.customerQueue.expanded).toBe(true);
+  });
+});
